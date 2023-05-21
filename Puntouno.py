d79d8113188ab9b34ac76831d88ee205d593f942
Jeimy <jeimyestefanias@gmail.com>
2023-05-20 21:18:03 -0500

    Actividad 6

---
+++ Puntouno.py
@@ -0,0 +1,15 @@
+#Punto uno
+from statistics import median
+from statistics import mean
+from statistics import mode
+from statistics import quantiles
+
+datos=[8, 8, 13, 15, 16, 16, 16, 16, 24, 29, 30, 30, 49, 55, 55, 60]
+
+print("La media de los datos es:" , mean(datos))
+print("La moda de los datos es:" , mode(datos))
+print("La mediana de los datos es:" , median(datos))
+
+percentil=quantiles(datos, n=100)
+print("El percentil de los datos es:", percentil)
+
